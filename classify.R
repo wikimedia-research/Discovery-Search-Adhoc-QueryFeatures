0254c14c6b893f12e6750d900579cd215fa8a7d5
Mikhail Popov <mpopov@wikimedia.org>
2016-05-12 18:26:23 -0700

    Added parameter tuning via validation

--- classify.R
+++ classify.R
@@ -14,34 +14,78 @@ source("refine.R") # yields: features_matrix
 standardize <- function(x) { return((x - mean(x))/sd(x)) }
 
 set.seed(20160512)
-subset_idx <- sample.int(nrow(queries), size = 11000, replace = FALSE)
+
+n_train <- 10000 # 10000
+n_validation <- 1000
+n_sets <- 100
+
+subset_idx <- sample.int(nrow(queries), size = n_train + n_validation * n_sets, replace = FALSE)
 queries_subset <- as.matrix(as.data.frame(queries)[subset_idx, c("zero_result", "n_terms", "n_chars", "n_feats")])
-queries_subset[, "n_terms"] <- standardize(log10(queries_subset[, "n_terms"]))
-queries_subset[, "n_chars"] <- standardize(log10(queries_subset[, "n_chars"]))
-queries_subset[, "n_chars"] <- standardize(sqrt(queries_subset[, "n_feats"]))
+queries_subset[, "n_terms"] <- standardize(log10(queries_subset[, "n_terms"] + 1))
+queries_subset[, "n_chars"] <- standardize(log10(queries_subset[, "n_chars"] + 1))
+queries_subset[, "n_feats"] <- standardize(sqrt(queries_subset[, "n_feats"]))
 # countries_subset <- dummy::dummy(as.data.frame(queries)[subset_idx, "country", drop = FALSE])
 features_matrix_subset <- as.matrix(features_matrix[subset_idx, ])
 temp <- cbind(queries_subset, features_matrix_subset) #, countries_subset)
 rm(queries_subset, features_matrix_subset, countries_subset); rm(subset_idx)
-validation_idx <- sample.int(nrow(temp), 1000, replace = FALSE)
+validation_idx <- sample.int(nrow(temp), n_validation * n_sets, replace = FALSE)
 # ^ will be used for out-of-bag validation
 remainder_idx <- setdiff(1:nrow(temp), validation_idx)
 training_idx <- sample(remainder_idx, floor(0.8 * length(remainder_idx)), replace = FALSE)
 test_idx <- setdiff(remainder_idx, training_idx)
 
-# distance_matrix <- dist(temp)
-# hc <- hclust(distance_matrix) # plot(hc)
+# We don't need no cross validation with the amount of data we have!
+library(progress)
+pb <- progress_bar$new(total = (ncol(temp)-1) * n_sets)
+validation_sets <- matrix(validation_idx, ncol = n_sets)
+misclassification_errors <- lapply(1:(ncol(temp)-1), function(m) {
+  rf <- randomForest(x = temp[training_idx, -1], xtest = temp[test_idx, -1],
+                     y = factor(temp[training_idx, 1], 0:1, c("some results", "zero results")),
+                     ytest = factor(temp[test_idx, 1], 0:1, c("some results", "zero results")),
+                     ntree = 200, samplesize = ceiling(0.6 * nrow(temp)), nodesize = 5, mtry = m,
+                     importance = FALSE, keep.forest = TRUE, proximity = FALSE)
+  misclassification_rates <- apply(validation_sets, 2, function(validation_set) {
+    # predictions <- predict(rf, temp[validation_set, -1], type = "vote", norm.votes = TRUE)
+    predictions <- predict(rf, temp[validation_set, -1], type = "response")
+    levels(predictions) <- union(levels(predictions), c("some results", "zero results"))
+    true_labels <- factor(temp[validation_set, 1], 0:1, c("some results", "zero results"))
+    # predicted_labels <- colnames(predictions)[apply(predictions, 1, which.max)]
+    confusion_matrix <- table(true_labels, predictions)
+    pb$tick()
+    return((confusion_matrix["some results", "zero results"] + confusion_matrix["zero results", "some results"])/length(validation_set))
+  })
+  return(data.frame(point.est = mean(misclassification_rates),
+                    std.dev = sd(misclassification_rates),
+                    lower = unname(quantile(misclassification_rates, 0.025)),
+                    upper = unname(quantile(misclassification_rates, 0.975))))
+}) %>% dplyr::bind_rows(.id = "m")
+misclassification_errors$m <- as.numeric(misclassification_errors$m)
+p <- ggplot(data = misclassification_errors,
+       aes(x = m, y = point.est)) +
+  geom_ribbon(aes(ymax = upper, ymin = lower), alpha = 0.1) +
+  geom_line() + geom_point() +
+  geom_errorbar(aes(ymax = point.est + std.dev, ymin = point.est - std.dev)) +
+  ggthemes::theme_tufte(base_family = "Gill Sans", base_size = 14) +
+  scale_y_continuous("Misclassification Error Rate", labels = scales::percent_format()) +
+  scale_x_continuous(breaks = misclassification_errors$m) +
+  labs(x = "Number of Variables Considered At Each Split",
+       subtitle = paste("Validating a random forest (trained on", n_train, "units) using", n_sets, "sets of", n_validation, "units each."),
+       title = "Misclassification using validation dataset.") +
+  geom_vline(xintercept = misclassification_errors$m[which.min(misclassification_errors$point.est)],
+             linetype = "dashed")
+print(p)
+ggsave("mis_rate.png", p, path = fig_path, units = "in", dpi = 150, height = 8, width = 12)
 
-set.seed(0)
+# misclassification_errors$m[which.min(misclassification_errors$point.est)] # 16?
 rf <- randomForest(x = temp[training_idx, -1], xtest = temp[test_idx, -1],
                    y = factor(temp[training_idx, 1], 0:1, c("some results", "zero results")),
                    ytest = factor(temp[test_idx, 1], 0:1, c("some results", "zero results")),
                    ntree = 200, samplesize = ceiling(0.6 * nrow(temp)),
-                   mtry = floor(sqrt(ncol(temp)-1)), # tune this with CV???
+                   nodesize = 5, mtry = 19, # mtry = floor(sqrt(ncol(temp)-1)), # tune with CV
                    importance = TRUE, keep.forest = TRUE, proximity = TRUE)
-# plot(rf)
-varImpPlot(rf); par(mfrow = c(1, 1))
-MDSplot(rf, fac = factor(temp[training_idx, 1], 0:1, c("some results", "zero results")))
+
+# plot(rf); varImpPlot(rf); par(mfrow = c(1, 1))
+# MDSplot(rf, fac = factor(temp[test_idx, 1], 0:1, c("some results", "zero results")))
 
 variable_importance <- data.frame(Variable = rownames(importance(rf)), importance(rf)) %>%
   set_rownames(NULL)
@@ -49,7 +93,7 @@ p1 <- ggplot(data = variable_importance,
              aes(x = reorder(Variable, -zero.results),
                  y = zero.results)) +
   geom_bar(stat = "identity", aes(fill = ifelse(zero.results >= 0, "Higher", "Lower"))) +
-  scale_fill_brewer(type = "qual", palette = "Set1", direction = -1,
+  scale_fill_manual(values = RColorBrewer::brewer.pal(3, "Set1")[2:1],
                     guide = guide_legend(title = "Chances of zero results")) +
   geom_hline(yintercept = 0) +
   coord_flip() +
@@ -68,9 +112,7 @@ p2 <- ggplot(data = variable_importance,
 p3 <- ggplot(data = variable_importance,
              aes(x = reorder(Variable, -MeanDecreaseAccuracy),
                  y = MeanDecreaseAccuracy)) +
-  geom_bar(stat = "identity", aes(fill = ifelse(MeanDecreaseAccuracy >= 0, "Better", "Worse"))) +
-  scale_fill_brewer(type = "qual", palette = "Set1", direction = -1,
-                    guide = guide_legend(title = "Accuracy")) +
+  geom_bar(stat = "identity") +
   geom_hline(yintercept = 0) +
   coord_flip() +
   ggthemes::theme_tufte(base_family = "Gill Sans", base_size = 14) +
@@ -80,11 +122,6 @@ p3 <- ggplot(data = variable_importance,
 p <- plot_grid(p1, p2, p3, nrow = 1)
 print(p)
 ggsave("var_imp.png", p, path = fig_path, units = "in", dpi = 150, height = 8, width = 24)
-
-predictions <- predict(rf, temp[validation_idx, -1], type = "vote", norm.votes = TRUE)
-true_labels <- as.character(factor(temp[validation_idx, 1], 0:1, c("some results", "zero results")))
-predicted_labels <- colnames(predictions)[apply(predictions, 1, which.max)]
-(confusion_matrix <- table(true_labels, predicted_labels))
 
 ## Note: look into parallel random forests -- https://bitbucket.org/mkuhn/parallelrandomforest
 ## See also: http://blog.mckuhn.de/2013/09/introducing-parallelrandomforest-faster.html
